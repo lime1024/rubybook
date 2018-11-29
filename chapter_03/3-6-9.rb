f3976408c66741b61f14d8db5439ffa879b0d704
lime1024 <lime1024@gmail.com>
2018-11-29 21:01:26 +0900

    Add chapter_03

---
+++ chapter_03/3-6-9.rb
@@ -0,0 +1,19 @@
+# 次のように表示するプログラムを繰り返すを使って書く
+# カフェラテ
+# モカ
+# モカ
+# カフェラテ
+# モカ
+# モカ
+# フラペチーノ
+caffelatte = "カフェラテ"
+moca = "モカ"
+frappuccino = "フラペチーノ"
+
+2.times do
+  puts caffelatte
+  2.times { puts moca } 
+end
+
+puts frappuccino
+
