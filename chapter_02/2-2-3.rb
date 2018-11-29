71b3750ff63347e00f1d9dc3a26ed8511c1d6371
lime1024 <lime1024@gmail.com>
2018-11-29 15:47:31 +0900

    Add 2-3 - 2-7

---
+++ chapter_02/2-2-3.rb
@@ -0,0 +1,3 @@
+# 文字列 "Ruby" を表示
+puts "Ruby"
+
---
+++ chapter_02/2-7-9_calc.rb
@@ -0,0 +1,9 @@
+# 300円のコーヒー2杯の値段を計算しようとすると、エラーを含むプログラムがある
+# エラーメッセージを参考にプログラムを正しく修正する
+# total = 300 * 2
+# puts t
+# => Traceback (most recent call last):
+# => 2-7-9_calc.rb:4:in `<main>': undefined local variable or method `t' for main:Object (NameError)
+total = 300 * 2
+puts total
+
